461c6c2305e2b61cdd86312d85f7046ee2a3c090
yuutotsuki <ootsuka1012@gmail.com>
2025-07-15 23:00:20 +0900

    不要ファイル削除とhandleSlackMessage.ts修正

--- utils.ts
+++
@@ -1,5 +0,0 @@
-export function redactSensitive(str: string): string {
-  return str
-    .replace(/Bearer [\w\-\.]+/g, 'Bearer ***')
-    .replace(/token["']?: ?["']?[\w\-\.]+["']?/gi, 'token: "***"');
-} 
